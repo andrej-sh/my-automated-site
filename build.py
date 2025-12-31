3f5c580adcc664c8cce72ab89a18f6a4cfc31bfb
andrej-sh <233600533+andrej-sh@users.noreply.github.com>
2025-12-31 16:32:35 +0300

    Update build.py

--- build.py
+++ build.py
@@ -1,15 +1,15 @@
- # build.py
-   import shutil, os
-   # Очищаем папку build (если есть)
-   shutil.rmtree('build', ignore_errors=True)
-   os.makedirs('build')
-   # Копируем и изменяем HTML
-   with open('src/index.html', 'r') as f:
-       html = f.read()
-   # Добавляем метку о сборке
-   html = html.replace('</body>', '<p>✅ Built by CI/CD</p>\n</body>')
-   with open('build/index.html', 'w') as f:
-       f.write(html)
-   # Копируем CSS
-   shutil.copy('src/style.css', 'build/style.css')
-   print("Build completed.")
+# build.py
+import shutil, os
+# Очищаем папку build (если есть)
+shutil.rmtree('build', ignore_errors=True)
+os.makedirs('build')
+# Копируем и изменяем HTML
+with open('src/index.html', 'r') as f:
+    html = f.read()
+# Добавляем метку о сборке
+html = html.replace('</body>', '<p>✅ Built by CI/CD</p>\n</body>')
+with open('build/index.html', 'w') as f:
+    f.write(html)
+# Копируем CSS
+shutil.copy('src/style.css', 'build/style.css')
+print("Build completed.")
